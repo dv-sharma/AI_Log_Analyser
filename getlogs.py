8d094cf192d50954475cd695a1e06c25dd54ae72
Divyam Sharma <65087388+dv-sharma@users.noreply.github.com>
2025-07-16 15:05:17 -0400

    Created getlogs.py for extracting log messages from CloudWatch

---
+++ getlogs.py
@@ -0,0 +1,40 @@
+import boto3
+import datetime
+import time
+
+client = boto3.client(
+    service_name="logs",  # Using the logs service
+    region_name="us-east-1"
+)
+
+end_time = datetime.datetime.utcnow()
+start_time = end_time - datetime.timedelta(hours=1)
+
+start_time_ms = int(start_time.timestamp() * 1000)
+end_time_ms = int(end_time.timestamp() * 1000)
+
+def get_logs():
+    response = client.get_log_events(
+        logGroupName='syslog',
+        logStreamName='i-004ef5cfe0a88b484',
+        startTime=start_time_ms,
+        endTime=end_time_ms,
+        limit=123,
+    )
+
+    for event in response['events']:
+        print(f"{event['timestamp']}: {event['message']}")
+
+""" def get_logs():
+    response = client.list_log_groups(
+        limit=10
+    )
+
+    for group in response['logGroups']:
+        print(group['logGroupName'])  """
+
+if __name__ == "__main__":
+    get_logs()
+
+    #et_logs()
+   #print("Listed log groups successfully.")
